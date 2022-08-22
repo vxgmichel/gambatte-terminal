ec4489a09471e5453f0881bd6e57a4668cf95c7f
Vincent Michel <vxgmichel@gmail.com>
2022-08-22 10:29:58 +0200

    Update asyncssh and prompt-toolkit dependencies

--- gambaterm/ssh_app_session.py
+++ gambaterm/ssh_app_session.py
@@ -14,77 +14,36 @@ from prompt_toolkit.output.vt100 import Vt100_Output
 from prompt_toolkit.application.current import create_app_session
 
 
-class StdoutFromProcess:
-    def __init__(self, process):
-        self._r, self._w = os.pipe()
-        self.process = process
-
-    def write(self, data):
-        os.write(self._w, data.replace(b"\n", b"\r\n"))
-
-    def isatty(self) -> bool:
-        return True
-
-    def flush(self):
-        pass
-
-    def get_size(self):
-        width, height, _, _ = self.process.get_terminal_size()
-        return Size(rows=height, columns=width)
-
-    def fileno(self):
-        return self._w
-
-    @property
-    def encoding(self):
-        return self.process._encoding
-
-    def close(self):
-        for fd in (self._r, self._w):
-            try:
-                os.close(fd)
-            except OSError:
-                pass
-
-
 @asynccontextmanager
 async def vt100_output_from_process(process):
-    stdout = StdoutFromProcess(process)
+    def get_size():
+        width, height, _, _ = process.get_terminal_size()
+        return Size(rows=height, columns=width)
+
     term = process.get_terminal_type()
-    vt100_output = Vt100_Output(stdout, stdout.get_size, term=term, write_binary=True)
-    try:
-        await process.redirect_stdout(stdout._r)
+    read_fd, write_fd = os.pipe()
+    with open(write_fd, "w", newline="\r\n") as stdout:
+        vt100_output = Vt100_Output(stdout, get_size, term=term)
+        await process.redirect_stdout(read_fd)
         try:
             yield vt100_output
         finally:
             await process.redirect_stdout(subprocess.PIPE)
             await asyncio.sleep(0)  # Let the loop remove the file descriptor
-    finally:
-        stdout.close()
 
 
 @asynccontextmanager
 async def vt100_input_from_process(process):
-    vt100_input = create_pipe_input()
-
-    # Patch `vt100_input` to ignore `OSError`s
-    def close(self):
-        for fd in (self._r, self._w):
-            try:
-                os.close(fd)
-            except OSError:
-                pass
-
-    vt100_input.close = close.__get__(vt100_input)
-    try:
-        await process.redirect_stdin(vt100_input._w)
+    with create_pipe_input() as vt100_input:
+        await process.redirect_stdin(vt100_input.pipe.write_fd)
         try:
             yield vt100_input
         finally:
+            # Prevent vt100_input.pipe.close from failing with an OSError
+            # as the write end of the pipe has already been closed by asyncssh
+            vt100_input.pipe._write_closed = True
             await process.redirect_stdin(subprocess.PIPE)
             await asyncio.sleep(0)  # Let the loop remove the file descriptor
-    finally:
-        vt100_input.close()
 
 
 @contextmanager
