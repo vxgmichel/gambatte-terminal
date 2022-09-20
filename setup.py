f7653ea15af2f4a37d205009b210cd7cf7cde532
Vincent Michel <vxgmichel@gmail.com>
2022-09-20 12:15:16 +0200

    Drop python 3.6 support

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     extras_require={
         "controller-support": ["pygame>=1.9.5"],
     },
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     entry_points={
         "console_scripts": [
             "gambaterm = gambaterm:main",
@@ -85,7 +85,6 @@ setup(
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
