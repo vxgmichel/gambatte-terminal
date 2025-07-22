04022dc442a396a818dde2090d67b4ea77853538
Ti Ribeiro Silverio <tirs@estudante.ufscar.br>
2025-07-22 13:35:48 -0300

    Added back X11 input handling if not on a Wayland compositor.

--- gambaterm/keyboard_input.py
+++ gambaterm/keyboard_input.py
@@ -4,7 +4,8 @@ import sys
 import time
 import logging
 import threading
-from contextlib import contextmanager
+import os
+from contextlib import contextmanager, closing
 from typing import Callable, Iterator, TYPE_CHECKING
 from prompt_toolkit.application import create_app_session
 
@@ -15,8 +16,19 @@ if sys.platform == "linux":
         import evdev
     except ImportError:
         evdev = None
+    try:
+        from Xlib import XK
+        from Xlib.ext import xinput
+        from Xlib.display import Display
+    except ImportError:
+        XK = None
+        xinput = None
+        Display = None
 else:
     evdev = None
+    XK = None
+    xinput = None
+    Display = None
 
 if TYPE_CHECKING:
     import pynput
@@ -61,6 +73,48 @@ def get_evdev_event_mapping(console: Console) -> dict[int, Console.Event]:
         evdev.ecodes.KEY_9: console.Event.SELECT_STATE_9,
         evdev.ecodes.KEY_L: console.Event.LOAD_STATE,
         evdev.ecodes.KEY_K: console.Event.SAVE_STATE,
+    }
+
+
+def get_xlib_input_mapping(console: Console) -> dict[int, Console.Input]:
+    return {
+        # Directions
+        XK.XK_Up: console.Input.UP,
+        XK.XK_Down: console.Input.DOWN,
+        XK.XK_Left: console.Input.LEFT,
+        XK.XK_Right: console.Input.RIGHT,
+        # A button
+        XK.XK_f: console.Input.A,
+        XK.XK_v: console.Input.A,
+        XK.XK_space: console.Input.A,
+        # B button
+        XK.XK_d: console.Input.B,
+        XK.XK_c: console.Input.B,
+        XK.XK_Alt_L: console.Input.B,
+        XK.XK_Alt_R: console.Input.B,
+        # Start button
+        XK.XK_Return: console.Input.START,
+        XK.XK_Control_R: console.Input.START,
+        # Select button
+        XK.XK_Shift_R: console.Input.SELECT,
+        XK.XK_Delete: console.Input.SELECT,
+    }
+
+
+def get_xlib_event_mapping(console: Console) -> dict[int, Console.Event]:
+    return {
+        XK.XK_0: console.Event.SELECT_STATE_0,
+        XK.XK_1: console.Event.SELECT_STATE_1,
+        XK.XK_2: console.Event.SELECT_STATE_2,
+        XK.XK_3: console.Event.SELECT_STATE_3,
+        XK.XK_4: console.Event.SELECT_STATE_4,
+        XK.XK_5: console.Event.SELECT_STATE_5,
+        XK.XK_6: console.Event.SELECT_STATE_6,
+        XK.XK_7: console.Event.SELECT_STATE_7,
+        XK.XK_8: console.Event.SELECT_STATE_8,
+        XK.XK_9: console.Event.SELECT_STATE_9,
+        XK.XK_l: console.Event.LOAD_STATE,
+        XK.XK_k: console.Event.SAVE_STATE,
     }
 
 
@@ -168,6 +222,83 @@ def evdev_key_pressed_context(
 
 
 @contextmanager
+def xlib_key_pressed_context(
+    display: str | None = None,
+) -> Iterator[Callable[[], set[int]]]:
+    if XK is None or xinput is None or Display is None:
+        raise RuntimeError("Xlib library not found.")
+
+    with closing(Display(display)) as xdisplay:
+        extension_info = xdisplay.query_extension("XInputExtension")
+        xinput_major = extension_info.major_opcode
+        # Set of currently pressed keys and focused flag
+        pressed: set[int] = set()
+        focused = True
+        # Save current focus, as it is likely to be the terminal window
+        term_window = xdisplay.get_input_focus().focus
+        term_window.xinput_select_events(
+            [(xinput.AllDevices, xinput.FocusInMask | xinput.FocusOutMask)]
+        )
+        # It is possible the select events directly on the terminal window, but for some
+        # reasons, the events won't be propagated for some terminals like kitty.
+        # Instead, we select the events on the root windows and then perform some
+        # filtering.
+        xdisplay.screen().root.xinput_select_events(
+            [(xinput.AllDevices, xinput.KeyPressMask | xinput.KeyReleaseMask)]
+        )
+
+        def get_pressed() -> set[int]:
+            nonlocal focused
+            # Loop over pending events
+            while xdisplay.pending_events():
+                event = xdisplay.next_event()
+                # Unexpected events
+                if not hasattr(event, "extension"):
+                    continue
+                if event.extension != xinput_major:
+                    continue
+                # Focus has been lost
+                if event.evtype == xinput.FocusOut:
+                    focused = False
+                    pressed.clear()
+                    continue
+                # Focus has been retrieved
+                if event.evtype == xinput.FocusIn:
+                    focused = True
+                    continue
+                # The window is currently not focused
+                if not focused:
+                    continue
+                # Extract key press/release information
+                keycode = event.data.detail
+                mods = event.data.mods.effective_mods
+                keysym = xdisplay.keycode_to_keysym(keycode, 0)
+                modkeysym = xdisplay.keycode_to_keysym(keycode, mods)
+                keystr = xdisplay.lookup_string(keysym)
+                repeat = event.data.flags & 0x10000
+                is_key_pressed = event.evtype == xinput.KeyPress and not repeat
+                is_key_released = event.evtype == xinput.KeyRelease
+                # Prepare info string
+                info_string = f"keycode={keycode}, keysym={keysym}, "
+                info_string += f"modkeysym={modkeysym}, keystr={keystr}"
+                # Update the `pressed` set accordingly
+                if is_key_pressed:
+                    pressed.add(keysym)
+                    logging.info("Key pressed: " + info_string)
+                if is_key_released:
+                    pressed.discard(keysym)
+                    logging.info("Key released: " + info_string)
+
+            # Return the currently pressed keys
+            return pressed
+
+        try:
+            yield get_pressed
+        finally:
+            pressed.clear()
+
+
+@contextmanager
 def pynput_key_pressed_context(
     display: str | None = None,
 ) -> Iterator[Callable[[], set[str]]]:
@@ -210,10 +341,16 @@ def console_input_from_keyboard_context(
     console: Console, display: str | None = None
 ) -> Iterator[InputGetter]:
     if sys.platform == "linux":
-        key_pressed_context = evdev_key_pressed_context
-        input_mapping = get_evdev_input_mapping(console)
-        event_mapping = get_evdev_event_mapping(console)
-        current_pressed: set[int] = set()
+        if os.environ.get("XDG_SESSION_TYPE") == "wayland":
+            key_pressed_context = evdev_key_pressed_context
+            input_mapping = get_evdev_input_mapping(console)
+            event_mapping = get_evdev_event_mapping(console)
+            current_pressed: set[int] = set()
+        else:
+            key_pressed_context = xlib_key_pressed_context
+            input_mapping = get_xlib_input_mapping(console)
+            event_mapping = get_xlib_event_mapping(console)
+            current_pressed: set[int] = set()
     else:
         key_pressed_context = pynput_key_pressed_context
         input_mapping = get_pynput_input_mapping(console)
@@ -234,7 +371,12 @@ def console_input_from_keyboard_context(
             if keysym in input_mapping
         }
 
-    with key_pressed_context() as get_pressed:
+    if key_pressed_context == evdev_key_pressed_context:
+        with key_pressed_context() as get_pressed:
+            yield get_input
+    else:
+        with key_pressed_context(display=display) as get_pressed:
+            yield get_input
         yield get_input
 
 
@@ -247,23 +389,52 @@ def main() -> None:
         with session.input.raw_mode():
             try:
                 session.output.hide_cursor()
-                with evdev_key_pressed_context() as get_pressed:
-                    while True:
-                        for key in session.input.read_keys():
-                            if key.key == "c-c":
-                                raise KeyboardInterrupt
-                            if key.key == "c-d":
-                                raise EOFError
-                        
-                        codes = [evdev.ecodes.KEY[code] for code in get_pressed()]
-                        
-                        print(*codes, flush=True, end="")
-                        
-                        time.sleep(1 / 30);
-                        
-                        session.output.write_raw("\r")
-                        session.output.erase_down()
-                        session.output.flush()
+                if os.environ.get("XDG_SESSION_TYPE") == "wayland":
+                    key_pressed_context = evdev_key_pressed_context
+                    mapping = lambda code: evdev.ecodes.KEY[code]
+                else:
+                    key_pressed_context = xlib_key_pressed_context
+                    reverse_lookup = {
+                        v: k[3:] for k, v in XK.__dict__.items() if k.startswith("XK_")
+                    }
+                    mapping = reverse_lookup.get
+
+                if os.environ.get("XDG_SESSION_TYPE") == "wayland":
+                    with evdev_key_pressed_context() as get_pressed:
+                        while True:
+                            for key in session.input.read_keys():
+                                if key.key == "c-c":
+                                    raise KeyboardInterrupt
+                                if key.key == "c-d":
+                                    raise EOFError
+                            
+                            codes = list(map(mapping, get_pressed()))
+                            
+                            print(*codes, flush=True, end="")
+                            
+                            time.sleep(1 / 30);
+                            
+                            session.output.write_raw("\r")
+                            session.output.erase_down()
+                            session.output.flush()
+                else:
+                    with xlib_key_pressed_context() as get_pressed:
+                        while True:
+                            for key in session.input.read_keys():
+                                if key.key == "c-c":
+                                    raise KeyboardInterrupt
+                                if key.key == "c-d":
+                                    raise EOFError
+                            
+                            codes = list(map(mapping, get_pressed()))
+                            
+                            print(*codes, flush=True, end="")
+                            
+                            time.sleep(1 / 30);
+                            
+                            session.output.write_raw("\r")
+                            session.output.erase_down()
+                            session.output.flush()
             except (KeyboardInterrupt, EOFError):
                 pass
             finally:
